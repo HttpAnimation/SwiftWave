11988b6b0ae90039169046c8d45c1f7adde18c9d
HttpAnimations <97435656+HttpAnimation@users.noreply.github.com>
2024-03-28 11:41:03 -0700

    Add files via upload

---
+++ SwiftWave/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftWave
+//
+//  Created by HttpAnimations on 09/01/1403 AP.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftWave/SwiftWaveApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftWaveApp.swift
+//  SwiftWave
+//
+//  Created by HttpAnimations on 09/01/1403 AP.
+//
+
+import SwiftUI
+
+@main
+struct SwiftWaveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
